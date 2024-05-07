d15f59df34bf7b8ce2e26980109d3135bb65cb12
Jeanbaptiste.pietri@skema.edu <Jeanbaptiste.pietri@skema.edu>
2024-05-07 13:01:33 +0000

    shipping_fee cleared

---
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_sales.sql
@@ -0,0 +1,20 @@
+with 
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'raw_gz_sales') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost,
+        ship_cost
+
+    from source
+)
+
+select * from renamed
